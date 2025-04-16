b756236a26f5ab55503c574f5944b51de8e6020a
Gurkgamer <gurkgamer@gmail.com>
2025-04-16 22:00:17 +0200

    feat: Clases autoload EventBus y Globals
    Se crean las clases globales EventBus y Globals.
    EventBus será la clase que tenga señales para eventos de interes para todo el proyecto.
    Globals tendrá variables comunes para todo el proyecto.

---
+++ pong/Global/Scripts/EventBus.gd
@@ -0,0 +1,7 @@
+extends Node
+
+## System signals
+
+## Game signals
+@warning_ignore("unused_signal") signal player_one_scored(goal_value: int)
+@warning_ignore("unused_signal") signal player_two_scored(goal_value: int)
