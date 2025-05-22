9d462f160510d55aff774944d95d2e0da77cd726
Gurkgamer <gurkgamer@gmail.com>
2025-05-22 05:29:29 +0200

    Simplificar UI puntuación

--- Scenes/UI/Score/score_ui.gd
+++ Scenes/UI/Score/score_ui.gd
@@ -1,33 +1,44 @@
-extends CanvasLayer
+class_name ScoreUI extends CanvasLayer
+
+@export var player1: Player
+@export var player2: Player
 
 @export var on_screen_score_time: float = 0.8
+
 @onready var screen_score_timer: Timer = %ScreenScoreTimer
 @onready var score_animation: AnimationPlayer = %ScoreAnimation
+@onready var left_player_score: Label = %LeftPlayerScore
+@onready var right_player_score: Label = %RightPlayerScore
 
 var player1_points: int = 0:
 	set(value):
 		player1_points = value
-		%LeftPlayerScore.text = str(player1_points)
+		left_player_score.text = str(player1_points)
 
 var player2_points: int = 0:
 	set(value):
 		player2_points = value
-		%RigthPlayerScore.text = str(player2_points)
+		right_player_score.text = str(player2_points)
 
 func _ready() -> void:
-	EventBus.player_one_scored.connect(_on_player_one_scored)
-	EventBus.player_two_scored.connect(_on_player_two_scored)
+	player1.scored.connect(_on_player_one_scored)
+	player2.scored.connect(_on_player_two_scored)
 	screen_score_timer.wait_time = on_screen_score_time
+	left_player_score.text = str(0)
+	right_player_score.text = str(0)
 
-func _on_player_one_scored(goal_points: int) -> void:
+func _on_player_one_scored() -> void:
 	await _show_score()
-	player1_points += goal_points
+	player1_points += 1
 	
-func _on_player_two_scored(goal_points:int) -> void:
+func _on_player_two_scored() -> void:
 	await _show_score()
-	player2_points += goal_points
+	player2_points += 1
 	
 func _show_score() -> void:
+	if score_animation.is_playing() and score_animation.current_animation == "hide_scoreboard":
+		await score_animation.animation_finished
+		
 	if screen_score_timer.is_stopped():
 		score_animation.play("show_scoreboard")
 		await score_animation.animation_finished
