fb9019d16e47fc0cc37d8393fc5bf1c655f7acd0
Gurkgamer <gurkgamer@gmail.com>
2025-05-22 05:28:48 +0200

    Nuevo control de la bola
    Ya no va por motor de fisica
    Se añade poder elegir angulo de comienzo
    Efecto de sonido al rebotar

--- Scenes/Actors/Ball/ball.gd
+++ Scenes/Actors/Ball/ball.gd
@@ -1,38 +1,72 @@
 class_name Ball extends Area2D
 
-enum possible_ball_owners{
-	NONE,
-	PLAYER1,
-	PLAYER2
-}
-
-@export var _ball_owner: possible_ball_owners = possible_ball_owners.NONE
+@onready var laser_pointer: Line2D = %LaserPointer
+@onready var bounce_sfx: AudioStreamPlayer = %BounceSFX
 
 var ball_direction: Vector2 = Vector2(1,1)
-var speed: float = 300.0
+var speed: float = 500.0
+var choosing_shoot_angle: bool = false
+var is_second_player: bool = false
+var is_bot: bool = false
+var player_starting: Player = null
 
+func _ready() -> void:
+	choosing_shoot_angle = true
+	laser_pointer.show()
+	
+func set_player_starting(player: Player, is_second_player_starting: bool) -> void:
+	player_starting = player
+	is_second_player = is_second_player_starting
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and choosing_shoot_angle:
+		choosing_shoot_angle = false
+		laser_pointer.hide()
+		
 func _physics_process(delta: float) -> void:
-	position = position + ball_direction * (speed * delta)
+	if not choosing_shoot_angle:
+		position = position + ball_direction * (speed * delta)
+	else:
+		draw_line_to_mouse()
+	
+func draw_line_to_mouse():
+	var start_point: Vector2 = global_position
+	var mouse_pointer: Vector2 = get_viewport().get_mouse_position()
+	var line_directon = (mouse_pointer - start_point).normalized()
+	if player_starting:
+		global_position = player_starting.get_ball_spawn_point()
+	
+	# No demasiado detras
+	if is_second_player and mouse_pointer.x > position.x - 30:
+		mouse_pointer.x = position.x - 30
+	elif not is_second_player and mouse_pointer.x < position.x + 30:
+		mouse_pointer.x = position.x + 30
+		
+	# No demasiado delante
+	var screen_middle = Global.screen_size / 2
+	if is_second_player and mouse_pointer.x < screen_middle.x:
+		mouse_pointer.x = screen_middle.x
+	elif not is_second_player and mouse_pointer.x > screen_middle.x:
+		mouse_pointer.x = screen_middle.x
+		
+	laser_pointer.points = [start_point, mouse_pointer]
+	ball_direction = Vector2(mouse_pointer - start_point).normalized()
 	
 # Paredes
 func _on_body_entered(body: Node) -> void:
 	if not body.is_in_group("Player"):
 		ball_direction.y = ball_direction.y * -1
-	else:
-		print("why though")
-
-func get_ball_owner() -> int:
-	return _ball_owner
+		bounce_sfx.play()
 
 # Porteria
 func _on_area_entered(area: Area2D) -> void:
-	print(area.name)
-	if not area.is_in_group("Player"):
-		ball_direction.x = ball_direction.x * -1
+	if area.is_in_group("Goal"):
+		queue_free()
 
 # Jugador
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("Player"):
+		bounce_sfx.play()
 		ball_direction.x = ball_direction.x * -1
 		match body_shape_index: # Que hacer dependiendo de la zona del palo
 			0:
