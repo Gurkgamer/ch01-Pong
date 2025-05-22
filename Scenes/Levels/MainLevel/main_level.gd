4c2b865566e749f07ed7ec8b61e16d4ed95fd917
Gurkgamer <gurkgamer@gmail.com>
2025-05-22 05:29:45 +0200

    Retoques proyecto

--- Scenes/Levels/MainLevel/main_level.gd
+++ Scenes/Levels/MainLevel/main_level.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@export var player1: Player
+@export var player2: Player
+
+func _ready() -> void:
+	player1.scored.connect(player2.spawn_ball)
+	player2.scored.connect(player1.spawn_ball)
