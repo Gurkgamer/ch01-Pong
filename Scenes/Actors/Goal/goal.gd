49b08d68f121c746ca3ae0068a4905816bd64593
Gurkgamer <gurkgamer@gmail.com>
2025-05-22 05:29:07 +0200

    SImplificar porteria

--- Scenes/Actors/Goal/goal.gd
+++ Scenes/Actors/Goal/goal.gd
@@ -1,32 +1,13 @@
 extends Area2D
 
-enum goal_sides{
-	RIGHT,
-	LEFT
-}
+@export var rival_player: Player
 
-@export var goal_side: goal_sides
-
-# Viejo
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Ball"):
-		_add_goal(body.get_ball_owner())
+@onready var scored_sfx: AudioStreamPlayer = %ScoredSFX
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Ball"):
-		_add_goal(area.get_ball_owner())
+		scored_sfx.play()
+		_score_goal()
 
-func _add_goal(goal_owner: int) -> void:
-	match goal_owner:
-		0:
-			if goal_side == goal_sides.RIGHT:
-				EventBus.player_one_scored.emit(1)
-			elif goal_side == goal_sides.LEFT:
-				EventBus.player_two_scored.emit(1)
-		1: # My owner is the player 1
-			if goal_side == goal_sides.RIGHT:
-				EventBus.player_one_scored.emit(1)
-		2: # My owner is PLayer 2
-			if goal_side == goal_sides.LEFT:
-				EventBus.player_two_scored.emit(1)
-	
+func _score_goal() -> void:
+	rival_player.scored.emit()
