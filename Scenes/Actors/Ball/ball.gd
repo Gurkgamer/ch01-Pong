b796ca26345788e186e8ac830251fa19a4138e65
Gurkgamer <gurkgamer@gmail.com>
2025-05-23 23:44:30 +0200

    Se mueve la parte de apuntar el disparo
    Se mueve la parte de apuntar para disparar la bola, de la bola al jugador

--- Scenes/Actors/Ball/ball.gd
+++ Scenes/Actors/Ball/ball.gd
@@ -1,57 +1,22 @@
 class_name Ball extends Area2D
 
-@onready var laser_pointer: Line2D = %LaserPointer
 @onready var bounce_sfx: AudioStreamPlayer = %BounceSFX
 
 var ball_direction: Vector2 = Vector2(1,1)
 var speed: float = 500.0
 var choosing_shoot_angle: bool = false
-var is_second_player: bool = false
-var is_bot: bool = false
-var player_starting: Player = null
 
 func _ready() -> void:
 	choosing_shoot_angle = true
-	laser_pointer.show()
-	
-func set_player_starting(player: Player, is_second_player_starting: bool) -> void:
-	player_starting = player
-	is_second_player = is_second_player_starting
-	
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and choosing_shoot_angle:
-		choosing_shoot_angle = false
-		laser_pointer.hide()
-		
+
 func _physics_process(delta: float) -> void:
 	if not choosing_shoot_angle:
 		position = position + ball_direction * (speed * delta)
-	else:
-		draw_line_to_mouse()
 	
-func draw_line_to_mouse():
-	var start_point: Vector2 = global_position
-	var mouse_pointer: Vector2 = get_viewport().get_mouse_position()
-	var line_directon = (mouse_pointer - start_point).normalized()
-	if player_starting:
-		global_position = player_starting.get_ball_spawn_point()
-	
-	# No demasiado detras
-	if is_second_player and mouse_pointer.x > position.x - 30:
-		mouse_pointer.x = position.x - 30
-	elif not is_second_player and mouse_pointer.x < position.x + 30:
-		mouse_pointer.x = position.x + 30
-		
-	# No demasiado delante
-	var screen_middle = Global.screen_size / 2
-	if is_second_player and mouse_pointer.x < screen_middle.x:
-		mouse_pointer.x = screen_middle.x
-	elif not is_second_player and mouse_pointer.x > screen_middle.x:
-		mouse_pointer.x = screen_middle.x
-		
-	laser_pointer.points = [start_point, mouse_pointer]
-	ball_direction = Vector2(mouse_pointer - start_point).normalized()
-	
+func set_ball_direction(ball_direction_selected: Vector2) -> void:
+	ball_direction = ball_direction_selected
+	choosing_shoot_angle = false
+
 # Paredes
 func _on_body_entered(body: Node) -> void:
 	if not body.is_in_group("Player"):
@@ -64,7 +29,7 @@ func _on_area_entered(area: Area2D) -> void:
 		queue_free()
 
 # Jugador
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_body_shape_entered(_body_rid: RID, body: Node2D, body_shape_index: int, _local_shape_index: int) -> void:
 	if body.is_in_group("Player"):
 		bounce_sfx.play()
 		ball_direction.x = ball_direction.x * -1
--- Scenes/Actors/Player/ball_spawn_point.gd
+++ Scenes/Actors/Player/ball_spawn_point.gd
@@ -3,17 +3,55 @@ extends Marker2D
 
 const BALL = preload("res://Scenes/Actors/Ball/ball.tscn")
 
-var _spawning: bool = false
+@onready var laser_pointer: Line2D = %LaserPointer
 
-func spawn_new_ball(is_bot: bool) -> void:
+var _spawning: bool = false
+var is_player2_bot: bool
+var is_player2_enabled: bool
+var selected_ball_direction: Vector2
+var spawning_ball: Node2D
+
+func spawn_new_ball(is_bot: bool, player2_enabled: bool) -> void:
+	is_player2_bot = is_bot
+	is_player2_enabled = player2_enabled
 	if _spawning:
 		return
 		
 	_spawning = true
+	laser_pointer.show()
 	
-	var new_ball = BALL.instantiate()
-	add_child(new_ball)	
-	new_ball.set_player_starting(get_parent(), is_bot)
-	new_ball.global_position = global_position
+	spawning_ball = BALL.instantiate()
+	add_child(spawning_ball)
+	spawning_ball.global_position = global_position
+
+func _process(_delta: float) -> void:
+	if _spawning:
+		draw_line_to_mouse()
+
+func draw_line_to_mouse():
+	var start_point: Vector2 = global_position
+	var mouse_pointer: Vector2 = get_viewport().get_mouse_position()
+	var line_directon = (mouse_pointer - start_point).normalized()
 	
-	_spawning = false
+	# No demasiado detras
+	if is_player2_bot and mouse_pointer.x > position.x - 30:
+		mouse_pointer.x = position.x - 30
+	elif not is_player2_bot and mouse_pointer.x < position.x + 30:
+		mouse_pointer.x = position.x + 30
+		
+	# No demasiado delante
+	var screen_middle = Global.screen_size / 2
+	if is_player2_bot and mouse_pointer.x < screen_middle.x:
+		mouse_pointer.x = screen_middle.x
+	elif not is_player2_bot and mouse_pointer.x > screen_middle.x:
+		mouse_pointer.x = screen_middle.x
+		
+	laser_pointer.points = [start_point, mouse_pointer]
+	selected_ball_direction = line_directon
+	spawning_ball.global_position = global_position
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and _spawning:
+		_spawning = false
+		laser_pointer.hide()
+		spawning_ball.set_ball_direction(selected_ball_direction)
