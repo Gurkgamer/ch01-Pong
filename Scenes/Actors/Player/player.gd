970f864cdd690b67cd0a82215d404c192105a647
Gurkgamer <gurkgamer@gmail.com>
2025-05-23 23:43:21 +0200

    Control de jugador 2
    Y primera pincelada a CPU

--- Scenes/Actors/Player/player.gd
+++ Scenes/Actors/Player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 signal scored()
 
 @export var is_bot: bool = false
+var is_player2_enabled: bool = false
 
 @onready var ball_spawn_point: BallSpawnPoint = %BallSpawnPoint
 
@@ -16,16 +17,28 @@ var down_key: String
 func _ready() -> void:
 	if not is_bot:
 		spawn_ball()
+	else:
+		EventBus.toggle_second_player.connect(
+			func(toggled_on: bool) -> void: is_player2_enabled = toggled_on)
 
 func _physics_process(delta: float) -> void:
 	if not is_bot:
 		var direction = Input.get_axis("player1_up", "player1_down")
 		velocity.y = direction * SPEED
-		
-		move_and_slide()
+	elif is_player2_enabled:
+		var direction = Input.get_axis("player2_up", "player2_down")
+		velocity.y = direction * SPEED
+	else:
+		_ai_move()
+	move_and_slide()
 		
 func get_ball_spawn_point() -> Vector2:
 	return ball_spawn_point.global_position
 		
 func spawn_ball() -> void:
-	ball_spawn_point.spawn_new_ball.call_deferred(is_bot)
+	ball_spawn_point.spawn_new_ball.call_deferred(is_bot, is_player2_enabled)
+	
+func _ai_move() -> void:
+	var direction = randf_range(-1,1)
+	velocity.y = direction * SPEED
+	move_and_slide()
